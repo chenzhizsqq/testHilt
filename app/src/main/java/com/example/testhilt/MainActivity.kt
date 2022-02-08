077869426338bd3f9d934e136747111233b40ab0
chenzhi <chenzhi@qq.com>
2022-02-08 14:50:46 +0900

    将依赖项注入 Android 类

--- app/src/main/java/com/example/testhilt/MainActivity.kt
+++ app/src/main/java/com/example/testhilt/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.testhilt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint      //将依赖项注入 Android 类
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
